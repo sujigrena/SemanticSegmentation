aa585d735ca0291b81d4b9f902ec484512d369eb
sujigrena <sujigrena@gmail.com>
2018-05-19 22:52:49 +0530

    updated

--- main.py
+++ main.py
@@ -139,7 +139,7 @@ def run():
 
         # TODO: Build NN using load_vgg, layers, and optimize function
         epochs = 50
-        batch_size = 5
+        batch_size = 4
 
         # TF placeholders
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
